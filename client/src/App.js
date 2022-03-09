4b35b5221fe22ccd0dc9a827eccd51f1c4181d3b
xaster-Kies <samueledward40@gmail.com>
2022-03-09 22:09:50 +0100

    post list rendered

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import PostCreate from './PostCreate'
+import PostList from './PostList'
+
 
 export default () => {
   return (
-  <div>
+  <div className='container'>
     Blog App
     <PostCreate/>
+    <hr />
+    <h1>Posts</h1>
+    <PostList/>
   </div>
   )
   
